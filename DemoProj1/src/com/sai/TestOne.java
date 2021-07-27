870ec1fd59278c1616d7d8c67a1624c4e719185d
Tl <Tl@gmail.com>
2021-07-27 11:34:12 +0530

    initial import

---
+++ DemoProj1/src/com/sai/TestOne.java
@@ -0,0 +1,7 @@
+package com.sai;
+
+public class TestOne {
+public static void main(String[] args) {
+	
+}
+}
---
+++ DemoProj1/src/com/utility/Test2.java
@@ -0,0 +1,10 @@
+package com.utility;
+
+import java.util.Date;
+
+public class Test2 {
+public Date getSysDate() {
+	return new Date();
+	
+}
+}
